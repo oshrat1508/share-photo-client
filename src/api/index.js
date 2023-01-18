e673c09730b9726ade311fb9fcd1f028dad09c0a
oshrat <oshbalay@gmail.com>
2023-01-18 11:08:29 +0200

    delete jquery

--- src/api/index.js
+++ src/api/index.js
@@ -1,25 +1,25 @@
 import axios from "axios";
 
 
-const API = axios.create({baseUrl:"https://picosh-server.onrender.com"})
+// const API = axios.create({baseUrl:"https://picosh-server.onrender.com"})
 
-API.interceptors.request.use((req)=>{
-if(localStorage.getItem('profile')){
-    req.headers.Authorization = `bearer ${JSON.parse(localStorage.getItem('profile')).token}`
-}
-return req
-})
+// API.interceptors.request.use((req)=>{
+// if(localStorage.getItem('profile')){
+//     req.headers.Authorization = `bearer ${JSON.parse(localStorage.getItem('profile')).token}`
+// }
+// return req
+// })
 
-export const fetchPosts = () => API.get('/posts');
-export const createPost = (newPost) => API.post('/posts', newPost);
-export const updatePost = (id , updatedPost) => API.patch(`/posts/${id}`, updatedPost);
-export const likePost = (id ) => API.patch(`/posts/${id}/likePost`);
-export const commentPost = (id ) => API.patch(`/posts/${id}/comments`);
-export const deletePost = (id ) => API.delete(`/posts/${id}`);
+export const fetchPosts = () => axios.get('https://picosh-server.onrender.com/posts');
+export const createPost = (newPost) => axios.post('https://picosh-server.onrender.com/posts', newPost);
+export const updatePost = (id , updatedPost) => axios.patch(`https://picosh-server.onrender.com/posts/${id}`, updatedPost);
+export const likePost = (id ) => axios.patch(`https://picosh-server.onrender.com/posts/${id}/likePost`);
+export const commentPost = (id ) => axios.patch(`https://picosh-server.onrender.com/posts/${id}/comments`);
+export const deletePost = (id ) => axios.delete(`https://picosh-server.onrender.com/posts/${id}`);
 
 
-export const singIn = (formData)=> API.post('/user/singin' , formData)
-export const singUp = (formData)=> API.post('/user/singup' , formData)
-export const updateUser = (email , updatedUser) => API.patch(`/user/${email}`, updatedUser);
-export const fetchUsers = () => API.get('/user');
-export const followUser = (id ) => API.patch(`/user/${id}/follow`);
+export const singIn = (formData)=> axios.post('https://picosh-server.onrender.com/user/singin' , formData)
+export const singUp = (formData)=> axios.post('https://picosh-server.onrender.com/user/singup' , formData)
+export const updateUser = (email , updatedUser) => axios.patch(`https://picosh-server.onrender.com/user/${email}`, updatedUser);
+export const fetchUsers = () => axios.get('https://picosh-server.onrender.com/user');
+export const followUser = (id ) => axios.patch(`https://picosh-server.onrender.com/user/${id}/follow`);
